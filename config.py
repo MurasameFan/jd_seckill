03bf6aafa08e10af4a33c0338a10805ce9c24cc0
fanjinhong <fanmurasame@gmail.com>
2022-02-17 02:05:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@
 GLOBAL_CONFIG = {
     "debug": False,
     # 正式抢购时间
-    "buy_time": "2021-01-28 10:00:00.000",
+    "buy_time": "2021-08-12 22:52:10.000", #"buy_time": "2021-08-18 12:00:00.000",
     # 每个账号抢购进程数
     "work_count": 8,
     # 茅台sku_id
-    "sku_id": "100012043978",
+    "sku_id": "71710673572", #"sku_id": "100013225917",
     # 账号列表
     "account_list": [
         {
-            "username": "xxxx",
-            "eid": "xxxxxxxxxxxx",
-            "fp": "xxxxxxxxxxxxxxx",
-            "payment_pwd": "",
-            "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+            "username": "15512181082_p",
+            "eid": "HZKG5NWQ7YDFQVVITXKTP767TO6LDOKIOF2GBOIAE7FU2YKU42O7R3DDUSZZZTKD53BJM5PLQ5B3JIO5NF45GAAYLI",
+            "fp": "c7257cb96034810946fa7d05f59a5517",
+            "payment_pwd": "950107",
+            "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
             "seckill_num": 2,
         },
     ]
